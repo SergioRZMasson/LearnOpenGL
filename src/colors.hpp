6bdd363da9c60ac92ca0f0c90a5d8dd21d22df44
Sergio Ricardo Zerbetto Masson <sergioze@microsoft.com>
2024-10-14 14:30:15 -0300

    first commit

---
+++ src/colors.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace Color 
+{
+    constexpr  glm::vec3 BurlyWood = glm::vec3(0.1, 0.1, 0.8);
+    constexpr  glm::vec3 SlateGray = glm::vec3(0.5, 0.5, 0.5);
+}
